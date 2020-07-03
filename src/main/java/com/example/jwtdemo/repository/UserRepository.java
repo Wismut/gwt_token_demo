68f56930f271a872c204229cfb75c3f7d9117876
Sergiy <asd@sed.dfs>
2020-07-03 17:16:15 +0300

    Implements UserServiceImpl

--- src/main/java/com/example/jwtdemo/repository/UserRepository.java
+++ src/main/java/com/example/jwtdemo/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.example.jwtdemo.repository;
 import com.example.jwtdemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.stream.Stream;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(Stream name);
+    User findByUsername(String name);
 }
--- src/main/java/com/example/jwtdemo/service/UserService.java
+++ src/main/java/com/example/jwtdemo/service/UserService.java
@@ -2,6 +2,16 @@ package com.example.jwtdemo.service;
 
 import com.example.jwtdemo.model.User;
 
+import java.util.List;
+
 public interface UserService {
-    User register();
+    User register(User user);
+
+    List<User> getAll();
+
+    User findByUsername(String username);
+
+    User findById(Long id);
+
+    void delete(Long id);
 }
