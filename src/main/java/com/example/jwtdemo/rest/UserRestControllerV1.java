4713224a51842bbe27d347b8813283969c56bfde
Sergiy <asd@sed.dfs>
2020-07-04 09:59:48 +0300

    Implements AdminUserDto

--- src/main/java/com/example/jwtdemo/rest/UserRestControllerV1.java
+++ src/main/java/com/example/jwtdemo/rest/UserRestControllerV1.java
@@ -2,6 +2,7 @@ package com.example.jwtdemo.rest;
 
 import com.example.jwtdemo.dto.UserDto;
 import com.example.jwtdemo.model.User;
+import com.example.jwtdemo.security.jwt.JwtTokenProvider;
 import com.example.jwtdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,21 +12,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/api/v1/users/")
 public class UserRestControllerV1 {
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserRestControllerV1(UserService userService) {
+    public UserRestControllerV1(UserService userService, JwtTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long id) {
+    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long id,
+                                               HttpServletRequest request) {
+        User userByToken = userService.findByUsername(jwtTokenProvider.getUsername(request.getHeader("authorization").substring(7)));
         User user = userService.findById(id);
         if (user == null) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        if (!Objects.equals(userByToken.getId(), user.getId())) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
         UserDto result = UserDto.fromUser(user);
         return new ResponseEntity<>(result, HttpStatus.OK);
